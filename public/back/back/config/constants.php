22669a9b5049635f2d2e09d58bda45622cffeb0e
Madeggg <madeg.le-mouel@isen-ouest.yncrea.fr>
2025-06-04 13:54:07 +0000

    Mode nuit

--- public/back/back/config/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Database constants.
-  define('DB_USER', '');
-  define('DB_PASSWORD', '');
-  define('DB_NAME', '');
-  define('DB_SERVER', 'localhost');
-  define('DB_PORT', '5432');
-?>
